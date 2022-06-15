2fd4dc7673acc5f2331edbafa069c19e2e03338e
Dayllan de Souza Alho <dayllan.alho@sou.inteli.edu.br>
2022-06-15 13:37:28 -0300

    Currículo para o Heroku

--- Semana 7/04_AUT_EST_EX_OBRIGATORIOS/SegundoExercicio.js
+++
@@ -1,7 +0,0 @@
-function inverter() {
-    var caixa1 = document.getElementById("caixa1").value;
-    var caixa2 = document.getElementById("caixa2").value;
-
-    document.getElementById("caixa1").value = caixa2;
-    document.getElementById("caixa2").value = caixa1;
-}
